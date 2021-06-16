716d9a5476e1e4c87da7fa193164709fe4353a77
asjustin83 <aasha.james@gmail.com>
2021-06-16 18:06:36 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='simulation_interview',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    version='0.1.0',
+    description='simulationproject for interview process',
+    author='Asha Justin',
+    license='',
+)
